41903a22fdffdc0b5a747186e98a0bd8a6a60e7b
Administrador <Administrador@192.168.12.190>
2015-11-18 10:33:07 +0100

    he introducido una linea

--- PrimerProyectoRepositorio/src/HelloWorld.java
+++ PrimerProyectoRepositorio/src/HelloWorld.java
@@ -1,9 +1,16 @@
+import java.util.Iterator;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("hola a todos");
+ 
+ for (int i = 0; i < 5; i++) {
+	System.out.println("hola");
+}
+	
+
 	}
 
 }
