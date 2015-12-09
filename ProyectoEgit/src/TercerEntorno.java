abf33923e995f62f0e1ceefdec58fed56a66c03f
Administrador <Administrador@WIN-L93IRKH2F9P>
2015-12-09 11:56:39 +0100

    copia del ejercicio

--- ProyectoEgit/src/TercerEntorno.java
+++ ProyectoEgit/src/TercerEntorno.java
@@ -1,8 +1,15 @@
-
+import java.util.*;
 public class TercerEntorno {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+Scanner sc = new Scanner(System.in);
+int numero1, numero2, resultado;
+System.out.print("Introduce primer número: ");
+numero1 = sc.nextInt();
+System.out.print("Introduce segundo número: ");
+numero2 = sc.nextInt();
 
 	}
 
